fbd04d2791e115139f864314f6a08628e11b6cb6
Robin Böhm <robinboehm@googlemail.com>
2014-01-17 16:28:01 +0100

    refactor(e2e): use protractor for e2e testing

--- test/e2e/cube/cube.e2e.js
+++ test/e2e/cube/cube.e2e.js
@@ -1,38 +1,27 @@
-describe("A initial project", function(){
+describe("A initial project", function () {
 
-    beforeEach(function(){
-        browser().navigateTo('/');
-    });
+  var webServerUrl = 'http://localhost:8080/';
+  beforeEach(function () {
+    browser.get(webServerUrl);
+  });
 
-    it("should include angularjs",function(){
-        expect(element('script').attr('src')).toBe('lib/angular/angular.js');
-    });
+  it("should include angularjs", function () {
+    expect(element(by.tagName('script')).getAttribute('src')).toBe(webServerUrl + 'lib/angular/angular.js');
+  });
 
-    it("should include cube styles",function(){
-        expect(element('link').attr('href')).toBe('styles/cube.css');
-    });
+  it("should include cube styles", function () {
+    expect(element(by.tagName('link')).getAttribute('href')).toBe(webServerUrl + 'styles/cube.css');
+  });
 
-    it("should contain three input-elements with bindings to x,y and z",function(){
-        expect(input('x').val()).toBeDefined();
-        expect(input('y').val()).toBeDefined();
-        expect(input('z').val()).toBeDefined();
-    });
+  it("should contain three input-elements with bindings to x,y and z", function () {
+    expect(element(by.model('x')).getAttribute("value")).toBeDefined();
+    expect(element(by.model('y')).getAttribute("value")).toBeDefined();
+    expect(element(by.model('z')).getAttribute("value")).toBeDefined();
+  });
 
-    it("should bind x,y,z to css attribute transform of the cube",function(){
-        var testTemplate = "-webkit-transform: rotateX({{x}}deg) rotateY({{y}}deg) rotate({{z}}deg)",
-            testString;
-
-        expect(element('.cube').css("-webkit-transform")).toBeDefined();
-
-        testString = testTemplate.replace("{{x}}","0").replace("{{y}}","0").replace("{{z}}","0");
-        expect(element('.cube').attr("style")).toBe(testString);
-
-        input('x').enter("1");
-        input('y').enter("2");
-        input('z').enter("3");
-
-        testString = testTemplate.replace("{{x}}","1").replace("{{y}}","2").replace("{{z}}","3");
-        expect(element('.cube').attr("style")).toBe(testString);
-    });
+  it("should contain a button-element with binding addCube", function () {
+    var button = element(by.css("button.addCube"));
+    expect(button.getAttribute("ng-click")).toBe("addCube(x,y,z)");
+  });
 
 });
