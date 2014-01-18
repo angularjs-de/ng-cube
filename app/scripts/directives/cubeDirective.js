6f4d1be53be95b4508889eb08b4be0bede428205
Robin Böhm <robinboehm@googlemail.com>
2014-01-18 17:26:45 +0100

    add(cube): start extending cube directive with flickr api

--- app/scripts/directives/cubeDirective.js
+++ app/scripts/directives/cubeDirective.js
@@ -1,12 +1,21 @@
 angular.module("cubeApp")
-    .directive("cube",function(){
+  .directive("cube", function (flickr) {
     return {
-        restrict: "E",
-        templateUrl: "views/cube.html",
-        scope: {
-            "x" : "@initX",
-            "y" : "@initY",
-            "z" : "@initZ"
+      restrict: "E",
+      templateUrl: "views/cube.html",
+      scope: {
+        x: "@initX",
+        y: "@initY",
+        z: "@initZ",
+        tag: "@"
+      },
+      link: function (scope) {
+        if (scope.tag) {
+          flickr.getPhotosByTag(scope.tag)
+            .then(function (imgArray) {
+              scope.img = imgArray;
+            });
         }
+      }
     };
-});
+  });
