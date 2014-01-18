4aea3419f37e839930aebfb86b4a1c9677949116
Robin Böhm <robinboehm@googlemail.com>
2014-01-18 18:50:40 +0100

    add(index): add new flickrService and tag-handling

---
+++ app/scripts/config/flickrConfig.js
@@ -0,0 +1,4 @@
+angular.module("cubeApp")
+    .config(function (flickrProvider) {
+        flickrProvider.apiKey("4e0e99bf242015aee01bffea1efff314");
+    });
--- app/scripts/controllers/cubeListCtrl.js
+++ app/scripts/controllers/cubeListCtrl.js
@@ -1,12 +1,12 @@
 angular.module("cubeApp")
-  .controller("CubeListCtrl", function ($scope) {
-    $scope.cubes = [];
+    .controller("CubeListCtrl", function ($scope) {
+      $scope.cubes = [];
 
-    $scope.addCube = function (xValue, yValue, zValue) {
-      $scope.cubes.push({x: xValue, y: yValue, z: zValue});
-    }
+      $scope.addCube = function (xValue, yValue, zValue, tagValue) {
+        $scope.cubes.push({x: xValue, y: yValue, z: zValue, tag: tagValue});
+      }
 
-    $scope.removeCube = function (index) {
-      $scope.cubes.splice(index, 1);
-    }
-  });
+      $scope.removeCube = function (index) {
+        $scope.cubes.splice(index, 1);
+      }
+    });
