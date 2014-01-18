d6113584fdf4d3075296e905cfb3aab8c32b641f
Robin Böhm <robinboehm@googlemail.com>
2014-01-18 12:25:33 +0100

    add(flickr): add flickr service impl

--- app/scripts/services/flickrService.js
+++ app/scripts/services/flickrService.js
@@ -1,10 +1,35 @@
+"use strict";
+
 angular.module("cubeApp")
-  .factory("flickr", function () {
+  .factory("flickr", function ($http) {
+    var getPhotosByTagFn = function (tag) {
+      return $http.get("http://api.flickr.com/services/rest/", {
+        params: {
+          method: "flickr.photos.search",
+          api_key: "API_KEY",
+          format: "json",
+          nojsoncallback: 1,
+          per_page: "6",
+          tags: tag,
+          tag_mode: "all"
+        }
+      })
+        .then(function (response) {
+          var array = response.data.photos.photo.map(function (photo) {
+            return "http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg"
+              .replace("{farm-id}", photo.farm)
+              .replace("{server-id}", photo.server)
+              .replace("{id}", photo.id)
+              .replace("{secret}", photo.secret);
+          });
+          return array;
+        });
+    };
 
     // Public API
     return {
-      getPhotosByTag: function () {
-
+      getPhotosByTag: function (tag) {
+        return getPhotosByTagFn(tag);
       }
     };
   });
--- test/unit/services/flickrService.spec.js
+++ test/unit/services/flickrService.spec.js
@@ -23,11 +23,37 @@ describe("A Flickr-Service", function () {
 
   describe("getPhotosByTag method", function () {
 
-    var flickrService;
+    var flickrService,
+      queryTag = "animal",
+      requestParams = {
+        method: "flickr.photos.search",
+        api_key: "API_KEY",
+        format: "json",
+        nojsoncallback: 1,
+        per_page: "6",
+        tag_mode: "all"
+      },
+      imgUrlFormat = "http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg",
+      flickrResponse = {
+        photos: {
+          photo: [
+            {
+              farm: "FARM",
+              server: "SERVER",
+              id: "ID",
+              secret: "SECRET"
+            }
+          ]
+        }
+      };
 
     beforeEach(inject(function (flickr) {
       flickrService = flickr;
     }));
+
+    beforeEach(function () {
+      $httpBackend.when('GET', 'http://api.flickr.com/services/rest/').respond(flickrResponse);
+    });
 
     it("should be defined", function () {
       expect(flickrService.getPhotosByTag).toBeDefined();
@@ -37,7 +63,86 @@ describe("A Flickr-Service", function () {
       expect(flickrService.getPhotosByTag instanceof Function).toBe(true);
     });
 
+    it("should request a http GET to the flicker rest API", function () {
+      $httpBackend.expectGET('http://api.flickr.com/services/rest/');
+      flickrService.getPhotosByTag(queryTag);
+      $httpBackend.flush();
+    });
+
+
+    it("should request the correct method", function () {
+      $httpBackend.expectGET('http://api.flickr.com/services/rest/', {method: requestParams.method});
+      flickrService.getPhotosByTag(queryTag);
+      $httpBackend.flush();
+    });
+
+    it("should request the correct api key", function () {
+      $httpBackend.expectGET('http://api.flickr.com/services/rest/', {api_key: requestParams.api_key});
+      flickrService.getPhotosByTag(queryTag);
+      $httpBackend.flush();
+    });
+
+    it("should request format json", function () {
+      $httpBackend.expectGET('http://api.flickr.com/services/rest/', {format: requestParams.format});
+      flickrService.getPhotosByTag(queryTag);
+      $httpBackend.flush();
+    });
+
+    it("should request nojsoncallback", function () {
+      $httpBackend.expectGET('http://api.flickr.com/services/rest/', {nojsoncallback: requestParams.nojsoncallback});
+      flickrService.getPhotosByTag(queryTag);
+      $httpBackend.flush();
+    });
+
+    it("should request per_page", function () {
+      $httpBackend.expectGET('http://api.flickr.com/services/rest/', {per_page: requestParams.per_page});
+      flickrService.getPhotosByTag(queryTag);
+      $httpBackend.flush();
+    });
+
+    it("should request the correct tags", function () {
+      $httpBackend.expectGET('http://api.flickr.com/services/rest/', {tags: queryTag});
+      flickrService.getPhotosByTag(queryTag);
+      $httpBackend.flush();
+    });
+
+    it("should request the correct tag_mode", function () {
+      $httpBackend.expectGET('http://api.flickr.com/services/rest/', {tag_mode: requestParams.tag_mode});
+      flickrService.getPhotosByTag(queryTag);
+      $httpBackend.flush();
+    });
+
+
+    it("should transform response to an array", function () {
+      var responseArray;
+      flickrService.getPhotosByTag(queryTag)
+        .then(function (response) {
+          responseArray = response;
+        });
+      $httpBackend.flush();
+
+      expect(responseArray instanceof Array).toBe(true);
+    });
+
+    it("should transform responseArray to correct flickr imgUrls", function () {
+      var responseArray;
+      flickrService.getPhotosByTag(queryTag)
+        .then(function (response) {
+          responseArray = response;
+        });
+      $httpBackend.flush();
+
+      var imgUrl = imgUrlFormat
+        .replace("{farm-id}", flickrResponse.farm)
+        .replace("{server-id}", flickrResponse.server)
+        .replace("{id}", flickrResponse.id)
+        .replace("{secret}", flickrResponse.secret);
+
+      expect(imgUrl).toBe(responseArray[0]);
+    });
+
   });
 
 
-});
+})
+;
