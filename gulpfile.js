372bf253053c8b4da0592061b3053972ec8f195e
Robin Böhm <robinboehm@googlemail.com>
2014-01-29 19:51:00 +0100

    chore(build): add build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,36 @@
 var gulp = require('gulp');
-var ngHtml2Js = require("gulp-ng-html2js");
+var ngHtml2Js = require('gulp-ng-html2js');
 var ngmin = require('gulp-ngmin');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
 
-gulp.task('default', function(){
-
-
-
-
-  gulp.src("./src/**/*.js")
+gulp.task('default', ['html2js', 'styles'], function () {
+  return gulp.src(['./src/**/*.js', './dist/views/*.js'])
       .pipe(ngmin())
-      .pipe(gulp.dest("./dist"));
-
-
-
-  gulp.src("./src/**/*.html")
-      .pipe(ngHtml2Js({
-        moduleName: "cubeApp",
-        prefix: "/src"
-      }))
-      .pipe(gulp.dest("./dist"));
+      .pipe(concat('ngcube.js'))
+      .pipe(gulp.dest('./'))
+      .pipe(concat('ngcube.min.js'))
+      .pipe(uglify({outSourceMaps: true}))
+      .pipe(gulp.dest('./'));
 });
+
+gulp.task('html2js', ['clean'], function () {
+  return gulp.src('./src/**/*.html')
+      .pipe(ngHtml2Js({
+        moduleName: 'cubeApp',
+        prefix: '/src'
+      }))
+      .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('./src/**/*.css')
+      .pipe(concat('ngcube.css'))
+      .pipe(gulp.dest('./'));
+})
+
+gulp.task('clean', function () {
+  return gulp.src('./dist', {read: false})
+      .pipe(clean());
+})
