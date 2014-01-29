e804edc0399461eecb95836bd9a76fe773921fd4
Robin Böhm <robinboehm@googlemail.com>
2014-01-29 19:00:06 +0100

    chore(gulp): add ng-min gulp plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
 var ngHtml2Js = require("gulp-ng-html2js");
+var ngmin = require('gulp-ngmin');
 
 gulp.task('default', function(){
+
+
+
+
+  gulp.src("./src/**/*.js")
+      .pipe(ngmin())
+      .pipe(gulp.dest("./dist"));
+
+
+
   gulp.src("./src/**/*.html")
       .pipe(ngHtml2Js({
         moduleName: "cubeApp",
